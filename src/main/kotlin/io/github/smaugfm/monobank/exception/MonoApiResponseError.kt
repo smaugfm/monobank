7a9c7965a7b67f9165f11cbe07ee511d1a7e78a2
Dmitry Marchuk <code@dmarchuk.com>
2023-03-24 14:37:48 +0200

    Update exception

--- src/main/kotlin/io/github/smaugfm/monobank/exception/MonoApiResponseError.kt
+++ src/main/kotlin/io/github/smaugfm/monobank/exception/MonoApiResponseError.kt
@@ -3,10 +3,6 @@ package io.github.smaugfm.monobank.exception
 import reactor.netty.http.client.HttpClientResponse
 
 class MonoApiResponseError(
-    val response: HttpClientResponse,
-    val body: ByteArray
-) : Error() {
-    override fun toString(): String {
-        return "${response.status()}\n${body.decodeToString()}"
-    }
-}
+    response: HttpClientResponse,
+    body: ByteArray
+) : Error("${response.status()}\n${body.decodeToString()}")
